e9acde1091046cf0c77c7943b49f756a15e49015
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-04-12 00:11:43 -0300

    finalizando o projeto

--- att19/index.js
+++ att19/index.js
@@ -1,5 +1,6 @@
 const textarea = document.querySelector('textarea')
 const tweetar = document.querySelector('button')
+const feed = document.querySelector(".conteudoPrincipal_listaTweets")
 
 function pegarTweet(event) {
     event.preventDefault()
@@ -26,9 +27,36 @@ function criarTweet(tweetTexto) {
         tempo : `${hora} : ${minutos}`
     }
 
-    listarTweet(tweet)
+    listarTemplateTweet(tweet)
 }
 
 function listarTweet(tweet) {
-    console.log(tweet)
+
+    const {nome,usuario,texto, tempo} = tweet
+
+    
+    //Criando elementos para listar o template
+    let li = document.createElement("li")
+    li.classList.add("conteudoPrincipal_tweet")
+    let img = document.createElement("img")
+    let div = document.createElement("div")
+    div.classList.add("tweet_conteudo")
+    let h2 = document.createElement("h2")
+    h2.innerText = nome
+    let p = document.createElement("p")
+    p.innerText = texto
+    let span = document.createElement("span")
+    span.innerText = `${usuario} - ${tempo}`
+
+    //Adicionando elementos dentro da DIV
+    div.appendChild(h2)
+    div.appendChild(span)
+    div.appendChild(p)
+
+    //Adicionando elementos dentro da LI
+    li.appendChild(img)
+    li.appendChild(div)
+
+    feed.appendChild(li)
+    textarea.innerText = ""
 }
